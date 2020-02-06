799434566df3850f8bf4eec8f8a8b15d97943786
Vivek Madarapu <vivek.madarapu.800@k12.friscoisd.org>
2020-02-06 16:26:37 -0600

    New Commit: Created HuffmanTree

--- src/HuffmanTree.java
+++ src/HuffmanTree.java
@@ -8,13 +8,11 @@ public class HuffmanTree {
     private int maxsize;
 
     public void huffHeapify(PriorityQueue<Integer> freq, HashMap<Integer, Character> data){
-        heap = new int[data.size()];
         while(freq.size() > 1){
             int min = freq.poll();
             if(freq.size() > 0){
                 int min2 = freq.poll();
             }
-            freq.add()
         }
 
     }
