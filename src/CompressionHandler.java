1b2d8b11dfd29e29c6bdc418f8b343a24b99da6d
VivekMadarapu <vivek.madarapu.800@k12.friscoisd.org>
2020-02-11 19:40:57 -0600

    New Commit: Set up UI

--- src/CompressionHandler.java
+++ src/CompressionHandler.java
@@ -1,19 +1,56 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class CompressionHandler {
 
     private HashMap<Object, Integer> freq = new HashMap<>();
+    File input;
 
     public CompressionHandler(String file) throws FileNotFoundException {
+        input = new File(file);
+        initFreq(file);
+    }
+
+    public void initFreq(String file) throws FileNotFoundException {
         Scanner reader = new Scanner(new File(file));
         reader.useDelimiter(".");
         while(reader.hasNext()){
-
+            Object o = reader.next();
+            if(!freq.containsKey(o)){
+                freq.put(o, 1);
+            }
+            else {
+                freq.put(o, freq.get(o)+1);
+            }
         }
+        freq.put("end", 1);
+    }
+
+    public void compress() throws IOException {
+        File out = new File(input.getName().substring(0, input.getName().length()-3) + "viv");
+        createFile(out);
+    }
+
+    public void decompress() throws IOException {
+        File out = new File(input.getName().substring(0, input.getName().length()-3) + "txt");
+        createFile(out);
+    }
+
+    public void createFile(File file) throws IOException {
+        int version = 0;
+        while (file.exists()) {
+            version++;
+            file = new File("(" + version + ") " + file.getName());
+        }
+        //noinspection ResultOfMethodCallIgnored
+        file.createNewFile();
+    }
+
+    public static void main(String[] args) {
+
     }
 
 }
--- src/UI.java
+++ src/UI.java
@@ -1,14 +1,38 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class UI {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner console = new Scanner(System.in);
-        String response = "";
+        String response;
+        CompressionHandler compressionHandler;
         do{
+            System.out.print("Compress (c) or Decompress (d)? (or 'q' to quit) >>> ");
+            response = console.next();
+            if(response.equals("c")){
+                System.out.print("Enter file name or 'q' to quit >>> ");
+                response = console.next();
+                if (response.equals("q")){
+                    break;
+                }
 
+                compressionHandler = new CompressionHandler(response);
+                compressionHandler.compress();
+                System.out.println(response + " has been compressed into " + response.substring(0, response.length()-3) + "viv");
+            }
+            else if(response.equals("d")){
+                System.out.print("Enter file name or 'q' to quit >>> ");
+                response = console.next();
+                if (response.equals("q")){
+                    break;
+                }
 
-
+                compressionHandler = new CompressionHandler(response);
+                compressionHandler.decompress();
+                System.out.println(response + " has been decompressed into " + response.substring(0, response.length()-3) + "txt");
+            }
 
         }while(!response.equals("q"));
     }
