74363389bb51f81f1100bb0d5bd1430769b2cbeb
Vivek Madarapu <vivek.madarapu.800@k12.friscoisd.org>
2020-02-12 15:54:38 -0600

    New Commit: Added Helper Classes

--- src/HuffmanTree.java
+++ src/HuffmanTree.java
@@ -1,13 +1,26 @@
-import com.sun.source.tree.BinaryTree;
-
-import java.util.HashMap;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class HuffmanTree {
-    public PriorityQueue<BinaryTree> tree;
+    public PriorityQueue<BinaryTree<Entry>> tree;
 
     public HuffmanTree(HashMap<Object, Integer> frequencies){
+        tree = new PriorityQueue<>();
+        Set<Object> keys = frequencies.keySet();
+        for (Object key : keys) {
+            tree.add(new BinaryTree(new Node<>(new Entry(key, frequencies.get(key)))));
+        }
+    }
 
+    public class Entry
+    {
+        public Object data;
+        public int frequency;
+
+        public Entry(Object data, int frequency)
+        {
+            this.data = data;
+            this.frequency = frequency;
+        }
     }
 
 
